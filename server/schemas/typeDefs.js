4f3cba8aa0ec026705e7252d58aa22c1ac1a56d0
Jack Linhart <iatenine@gmail.com>
2021-10-05 00:09:09 -0600

    Added Apollo Server to Express

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,15 +1,7 @@
 const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
-  type User{
-    _id: ID!
-    username: String!
-    email: String!
-    bookCount: Int!
-    savedBooks [Book!]!
-  }
-
-  type Book{
+  type Book {
     bookId: Int!
     title: String!
     description: String!
@@ -18,20 +10,35 @@ const typeDefs = gql`
     link: String!
   }
 
-  type Auth{
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    bookCount: Int!
+    savedBooks: [Book!]!
+  }
+
+  type Auth {
     token: String!
     user: User!
   }
 
   type Query {
-      me: User
+    me: User
   }
 
   type Mutation {
-      login(email: String!, password: String!): Auth
-      addUser(username: String!, email: String!, password:String!): Auth
-      saveBook(bookAuthors:[String!]!, description: String!, title: String!, bookId: Int!, image: String!, link: String!): User
-      removeBook(bookId: Int!): User
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(
+      bookAuthors: [String!]!
+      description: String!
+      title: String!
+      bookId: Int!
+      image: String!
+      link: String!
+    ): User
+    removeBook(bookId: Int!): User
   }
 `;
 
--- server/server.js
+++ server/server.js
@@ -1,21 +1,41 @@
-const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
+const express = require("express");
+const path = require("path");
+const db = require("./config/connection");
+const { ApolloServer } = require("apollo-server-express");
+const routes = require("./routes");
+const { typeDefs, resolvers } = require("./schemas");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+const init = async () => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  await server.start();
+
+  server.applyMiddleware({ app });
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+
+  // if we're in production, serve client/build as static assets
+  if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../client/build")));
+  }
+
+  app.use(routes);
+
+  db.once("open", () => {
+    app.listen(PORT, () =>
+      console.log(`🌍 Now listening on localhost:${PORT}`)
+    );
+  });
+};
+
+try {
+  init();
+} catch (error) {
+  console.error(error);
 }
-
-app.use(routes);
-
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
