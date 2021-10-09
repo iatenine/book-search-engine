72263e4da418da8cc6dd5fcada8b8a5dc9e9d5ba
Jack Linhart <iatenine@gmail.com>
2021-10-09 13:24:53 -0600

    Added Apollo Client Provider

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+import App from "./App";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql,
+} from "@apollo/client";
+
+const apolloUri =
+  "https://book-search-engine-server.herokuapp.com/graphql" ||
+  "http://localhost:3001/graphql";
+
+const client = new ApolloClient({
+  uri: apolloUri,
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ApolloProvider>,
+  document.getElementById("root")
 );
