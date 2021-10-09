7d885a6ea44415ecc10f9673e73d8f5d5301253e
Jack Linhart <iatenine@gmail.com>
2021-10-09 10:31:09 -0600

    Server build issues fixed

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const db = require("./config/connection");
 const { ApolloServer } = require("apollo-server-express");
 const routes = require("./routes");
 const { typeDefs, resolvers } = require("./schemas");
+const { authMiddleware } = require("./utils/auth");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -12,6 +13,7 @@ const init = async () => {
   const server = new ApolloServer({
     typeDefs,
     resolvers,
+    context: authMiddleware,
   });
 
   await server.start();
@@ -20,10 +22,8 @@ const init = async () => {
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
 
-  // if we're in production, serve client/build as static assets
-  if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../client/build")));
-  }
+  // Serve static assets regardles to ensure app will run properly on Heroku before pushing
+  app.use(express.static(path.join(__dirname, "../client/build")));
 
   app.use(routes);
 
